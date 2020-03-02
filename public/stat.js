ab27c124546271c7c1a718b7748f334f82393469
jimmy <you@example.com>
2020-03-02 15:08:48 +0800

    样式小调整

--- public/stat.js
+++ public/stat.js
@@ -1,1 +1,7 @@
-﻿
+﻿var _hmt = _hmt || [];
+(function () {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?4a2ccf4e03312b587e3e5915062e1441";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
