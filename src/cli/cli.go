0db3cf70eb2063f1cc9579412a861ce6b27863ef
jimmy <you@example.com>
2020-03-20 21:34:57 +0800

    add gin and go-micro

--- src/cli/cli.go
+++ src/cli/cli.go
@@ -9,15 +9,17 @@ import (
 )
 
 var cli client.Client
+var name string
 
 func init() {
 	service := micro.NewService()
 	service.Init()
 	cli = service.Client()
+	name = config.Get("srv").String("micro.hrefs.srv")
 }
 
 func Call(method string, req interface{}, rsp interface{}) error {
-	request := cli.NewRequest(config.Get("srv").String("micro.hrefs.srv"), fmt.Sprintf("Hrefs.%s", method), req, client.WithContentType("application/json"))
+	request := cli.NewRequest(name, fmt.Sprintf("Hrefs.%s", method), req, client.WithContentType("application/json"))
 
 	if err := cli.Call(context.TODO(), request, &rsp); err != nil {
 		fmt.Println(err)
--- src/srv/run.go
+++ src/srv/run.go
@@ -1,11 +1,15 @@
 package srv
 
 import (
+	"context"
+	"github.com/kataras/golog"
 	"github.com/micro/go-micro/v2"
 	"github.com/micro/go-micro/v2/config"
 	"github.com/micro/go-micro/v2/registry"
+	"github.com/micro/go-micro/v2/server"
 	"github.com/micro/go-plugins/registry/consul/v2"
 	"hrefs.cn/src/utils"
+	"time"
 )
 
 type Hrefs struct{}
@@ -19,9 +23,21 @@ func Start() {
 	service := micro.NewService(
 		micro.Registry(reg),
 		micro.Name(config.Get("srv").String("micro.hrefs.srv")),
+		micro.WrapHandler(logWrapper),
 	)
-
-	service.Init()
+	server.Init()
+	service.Server().Init(server.Wait(nil))
 	micro.RegisterHandler(service.Server(), new(Hrefs))
 	service.Run()
+}
+
+func logWrapper(fn server.HandlerFunc) server.HandlerFunc {
+	start := time.Now()
+	return func(ctx context.Context, req server.Request, rsp interface{}) error {
+		err := fn(ctx, req, rsp)
+		utils.WriteErrorLog(req.Endpoint(), err)
+
+		golog.Infof("%s %s", time.Since(start), req.Endpoint())
+		return err
+	}
 }
